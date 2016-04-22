117533729d4faa6deba6ab5e397bf2aec61ed7cc
timeloveboy <734991033@qq.com>
2016-04-22 18:49:03 +0800

    ~

---
+++ app/src/main/java/xyz/moechat/android/main/discover/fragment_discover.java
@@ -0,0 +1,20 @@
+package xyz.moechat.android.main.discover;
+
+import xyz.moechat.android.R;
+import xyz.moechat.android.base.basefragment;
+
+/**
+ * Created by timeloveboy on 16/4/13.
+ */
+public class fragment_discover extends basefragment{
+    public static fragment_discover fragment=new fragment_discover();
+    public static fragment_discover newInstance(){
+        return fragment;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_favorities;
+    }
+
+}
---
+++ app/src/main/java/xyz/moechat/android/main/my/fragment_my.java
@@ -0,0 +1,20 @@
+package xyz.moechat.android.main.my;
+
+import xyz.moechat.android.R;
+import xyz.moechat.android.base.basefragment;
+
+/**
+ * Created by timeloveboy on 16/4/13.
+ */
+public class fragment_my extends basefragment {
+    public static fragment_my fragment=new fragment_my();
+    public static fragment_my newInstance(){
+        return fragment;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_favorities;
+    }
+
+}
